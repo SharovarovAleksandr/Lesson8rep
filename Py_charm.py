7522b6e7c2e162180a46f92b567666706927b27d
Sharovarov Aleksandr <sharovarovaleksandr@gmail.com>
2022-11-14 18:43:15 +0200

    Final edition of program

--- Py_charm.py
+++ Py_charm.py
@@ -1,26 +1,26 @@
 import math
-def main_function(a,b):
+def main_function_triangl(a,b):
     s=a*b/2
     perim=a+b+math.sqrt(a*a+b*b)
     return s, perim
 
-def test_function():
+def test_function_triangl():
     while True:
         a = int(input("Введіть довжину одного катета прямокутного трикутника: "))
         if a>0:
             break
-        esle:
-        print("Катет трикутника не може бути менше або дорівнювати 0")
+        else:
+            print("Катет трикутника не може бути менше або дорівнювати 0")
     while True:
         b = int(input("Введіть довжину другого катета прямокутного трикутника: "))
         if b>0:
             break
-        esle:
-        print("Катет трикутника не може бути менше або дорівнювати 0")
+        else:
+            print("Катет трикутника не може бути менше або дорівнювати 0")
     return a,b
 
 print("Ця программа вираховує площу та периметр прямокутньго трикутника за двома катетами")
-c=test_function()
-d=main_function(c[0],c[1])
+c=test_function_triangl()
+d=main_function_triangl(c[0],c[1])
 print(f'Площа трикутника= {d[0]}')
 print(f'Периметр трикутника= {d[1]}')
